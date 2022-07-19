fb892212629931498bbdc9c58a66187dbb4ba211
petrostrak <pit.trak@gmail.com>
2022-07-19 18:52:14 +0300

    Add TestSQLiteRepository_InsertHolding

--- repository/db-sqlite_test.go
+++ repository/db-sqlite_test.go
@@ -1,11 +1,32 @@
 package repository
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestSQLiteRepository_Migrate(t *testing.T) {
 	err := testRepo.Mirate()
 
 	if err != nil {
 		t.Error("migrate failed:", err)
+	}
+}
+
+func TestSQLiteRepository_InsertHolding(t *testing.T) {
+	h := Holdings{
+		Amount:        1,
+		PurchaseDate:  time.Now(),
+		PurchasePrice: 1000,
+	}
+
+	result, err := testRepo.InsertHolding(h)
+
+	if err != nil {
+		t.Error("insert failed:", err)
+	}
+
+	if result.ID <= 0 {
+		t.Error("invalid id sent back:", result.ID)
 	}
 }
