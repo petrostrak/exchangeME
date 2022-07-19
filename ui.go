1e36e7bed81e0b852c2d135795fdc52504ca2136
petrostrak <pit.trak@gmail.com>
2022-07-19 13:18:41 +0300

    Creating and returning the prices container

--- ui.go
+++ ui.go
@@ -1,9 +1,13 @@
 package main
 
+import "fyne.io/fyne/v2/container"
+
 func (c *Config) makeUI() {
 	// get current price of gold
+	openPrice, currentPrice, priceChange := c.getPriceText()
 
 	// put price info into a container
+	priceContent := container.NewGridWithColumns(3, openPrice, currentPrice, priceChange)
 
 	// add container to window
 }
