e1502d4a74c2b7fd142d14e371c1416246bb3cce
petrostrak <pit.trak@gmail.com>
2022-07-23 17:47:00 +0300

    Building the table widget

--- holdings-tab.go
+++ holdings-tab.go
@@ -5,6 +5,9 @@ import (
 	"strconv"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/petrostrak/exchangeME/repository"
 )
@@ -14,7 +17,46 @@ func (c *Config) holdingsTab() *fyne.Container {
 }
 
 func (c *Config) getHoldingsTable() *widget.Table {
-	return nil
+	data := c.getHoldingSlice()
+
+	t := widget.NewTable(
+		func() (int, int) {
+			return len(data), len(data[0])
+		},
+		func() fyne.CanvasObject {
+			return container.NewVBox(widget.NewLabel(""))
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			if i.Col == len(data[0])-1 && i.Row != 0 {
+				// last cell in row, put a button
+				w := widget.NewButtonWithIcon("Delete", theme.DeleteIcon(), func() {
+					dialog.ShowConfirm("Delete?", "", func(deleted bool) {
+						id, _ := strconv.Atoi(data[i.Row][0].(string))
+						err := c.DB.DeleteHolding(int64(id))
+						if err != nil {
+							c.ErrorLog.Println(err)
+						}
+
+						// TODO: refresh the holdings table
+					}, c.MainWindow)
+				})
+				w.Importance = widget.HighImportance
+
+				o.(*fyne.Container).Objects = []fyne.CanvasObject{w}
+			} else {
+				// we are putting textual information
+				o.(*fyne.Container).Objects = []fyne.CanvasObject{
+					widget.NewLabel(data[i.Row][i.Col].(string)),
+				}
+			}
+		})
+
+	colwidth := []float32{50, 200, 200, 200, 110}
+	for i := 0; i < len(colwidth); i++ {
+		t.SetColumnWidth(i, colwidth[i])
+	}
+
+	return t
 }
 
 func (c *Config) getHoldingSlice() [][]any {
